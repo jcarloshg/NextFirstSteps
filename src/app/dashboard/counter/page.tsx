ef03bc74621f4f2104b3229eaf2e7c78ec7e9189
jcarloshg <carlosj12336@gmail.com>
2024-02-14 14:24:28 -0600

    finish section 4

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata: Metadata = {
+    title: 'Counter Page',
+    description: 'Counter Page - Description',
+};
+
+export default function CounterPage() {
+
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+
+            <span>Productos en el carrtio</span>
+
+            <CartCounter value={20} />
+
+        </div>
+    );
+}
